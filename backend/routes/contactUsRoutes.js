7143fe537477345dce933a7a1e0165a276807a6c
Hari Krishnan <harip@dematech.in>
2026-08-04 14:53:56 +0530

    Restructuring of files for gallery

--- backend/routes/contactUsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { contactUs } from "../controllers/contactUsController.js";
-import turnstileMiddleware from "../middleware/turnstileMiddleware.js";
-
-const router = express.Router();
-
-router.post("/", turnstileMiddleware, contactUs);
-
-export default router;
